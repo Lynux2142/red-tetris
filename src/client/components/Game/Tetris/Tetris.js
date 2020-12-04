e81d5fdb96cc14571fe4948342cceb4a658934f0
Lynux2142 <lguiller@student.42.fr>
2020-12-04 15:05:02 +0100

    ajout d'un useState de position et d'une fonction move qui efface au fure et a mesure

--- src/client/components/Game/Tetris/Tetris.js
+++ src/client/components/Game/Tetris/Tetris.js
@@ -6,23 +6,38 @@ const Tetris = () => {
   const HEIGHT = 20;
   const color = ['white', 'black'];
   const [grid, setGrid] = useState(new Array(HEIGHT).fill().map((line) => new Array(WIDTH).fill(0)));
-  const [tmpY, setTmpY] = useState(0);
-  const [tmpX, setTmpX] = useState(4);
   const [HTMLgrid, setHTMLgrid] = useState([]);
+  const [position, setPosition] = useState({x: 4, y: HEIGHT - 1});
+
+  const move = (initial, next) => {
+    grid[initial.y][initial.x] = 0;
+    grid[next.y][next.x] = 1;
+    setGrid([...grid]);
+  };
 
   const handlerKeydown = (e) => {
-    if (e.key === 'ArrowLeft' && tmpX > 0) {
-      setTmpX(tmpX - 1);
-    } else if (e.key === 'ArrowRight' && tmpX < WIDTH - 1) {
-      setTmpX(tmpX + 1);
+    if (e.key === 'ArrowLeft' && position.x > 0) {
+      move(position, {...position, x: position.x - 1});
+      setPosition({...position, x: position.x - 1});
+    } else if (e.key === 'ArrowRight' && position.x < WIDTH - 1) {
+      move(position, {...position, x: position.x + 1});
+      setPosition({...position, x: position.x + 1});
+    }
+  };
+
+  const handleTimer = () => {
+    if (position.y + 1 < HEIGHT) {
+      move(position, {...position, y: position.y + 1});
+      setPosition({...position, y: position.y + 1});
+    } else {
+      move(position, {...position, y: 0});
+      setPosition({...position, y: 0});
     }
   };
 
   useEffect(() => {
     document.addEventListener('keydown', handlerKeydown);
-    return (() => {
-      document.removeEventListener('keydown', handlerKeydown);
-    });
+    return (() => document.removeEventListener('keydown', handlerKeydown));
   });
 
   useEffect(() => {
@@ -40,19 +55,10 @@ const Tetris = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      grid[tmpY][tmpX] = 0;
-      if (tmpY + 1 < HEIGHT) {
-        setTmpY(tmpY + 1);
-      } else {
-        setTmpY(0);
-      }
+      handleTimer();
     }, 1000);
-    grid[tmpY][tmpX] = 1;
-    setGrid([...grid]);
-    return (() => {
-      clearInterval(interval);
-    });
-  }, [tmpY, tmpX]);
+    return (() => clearInterval(interval));
+  }, [position]);
 
   return (
     <div>
