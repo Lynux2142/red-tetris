becddcaa808152621f4f80a23d296af6865b599a
Myriam <myriam@heypongo.com>
2020-12-23 17:44:50 +0100

    REM: erasing all project

--- src/client/containers/context.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import socket from './socket.js';
-
-const SocketContext = React.createContext(socket);
-
-export default SocketContext;
--- src/client/containers/socket.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import io from 'socket.io-client';
-import params from '../../../params.js';
-
-const socket = io(params.server.url);
-
-export default socket;
