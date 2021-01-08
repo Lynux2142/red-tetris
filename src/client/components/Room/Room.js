d4cc0893c222e5198b761719b5a300ecc91e8abe
Lynux2142 <lguiller@student.42.fr>
2021-01-08 15:28:28 +0100

    ajout de l'affichage du nombre de joueur dans les rooms

--- src/client/components/Room/Room.js
+++ src/client/components/Room/Room.js
@@ -36,7 +36,7 @@ const Room = () => {
         Object.keys(rooms).map((key, i) => {
           return (
             <li className='list-group-item p-1' key={i}>
-              <label><b>{rooms[key].name}</b></label>
+              <label><b>{rooms[key].name}</b> <b>({Object.keys(rooms[key].players).length})</b></label>
               <button className='btn btn-danger float-right' onClick={() => joinRoom(rooms[key].name)}>Join</button>
             </li>
           );
