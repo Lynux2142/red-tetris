78ed505e3ad81ea708b18e3aaf4d9b1d3f5a6de2
Myriam <myriam@heypongo.com>
2021-01-22 17:53:13 +0100

    MOD: tetriminos.js > color in rgba format

--- src/server/tetriminos.js
+++ src/server/tetriminos.js
@@ -9,43 +9,43 @@ class Tetrisminos {
 
 class I extends Tetrisminos {
   constructor(position) {
-    super([{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}], position, {x: 1.5, y: 1.5}, 'lightblue');
+    super([{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 3, y: 1}], position, {x: 1.5, y: 1.5}, '80, 227, 230');
   }
 }
 
 class J extends Tetrisminos {
   constructor(position) {
-    super([{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, 'blue');
+    super([{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, '36, 95, 223');
   }
 }
 
 class L extends Tetrisminos {
   constructor(position) {
-    super([{x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, 'orange');
+    super([{x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, '223, 173, 36');
   }
 }
 
 class O extends Tetrisminos {
   constructor(position) {
-    super([{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}], position, {x: 0.5, y: 0.5}, 'yellow');
+    super([{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}], position, {x: 0.5, y: 0.5}, '223, 217, 36');
   }
 }
 
 class S extends Tetrisminos {
   constructor(position) {
-    super([{x: 1, y: 0}, {x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}], position, {x: 1, y: 1}, 'green');
+    super([{x: 1, y: 0}, {x: 2, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}], position, {x: 1, y: 1}, '48, 211, 56');
   }
 }
 
 class T extends Tetrisminos {
   constructor(position) {
-    super([{x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, 'purple');
+    super([{x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, '132, 61, 198');
   }
 }
 
 class Z extends Tetrisminos {
   constructor(position) {
-    super([{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, 'red');
+    super([{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}], position, {x: 1, y: 1}, '227, 78, 78');
   }
 }
 
